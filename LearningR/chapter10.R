be4dfa8b8534d33dda5f337edb130ab1b044b749
Nick <mxr1106go@hotmail.com>
2016-04-05 20:42:00 +0800

    add the contents of installr:update R and packages

--- LearningR/chapter10.R
+++ LearningR/chapter10.R
@@ -43,3 +43,24 @@ R.home('library')
 # 维护包
 #可以使用update.package(ask=FALSE)来更新包
 #使用remove.package('zoo')来删除包
+
+
+# 更新到R的最新版本，同时更新R包到最新版本
+# Through the 'installr' package you can automate the updating of R 
+# (on Windows, using updateR()) and install new software. 
+# Software installation is initiated through a GUI (just run installr()), or through functions such as: install.Rtools(), install.pandoc(), install.git(), and many more. 
+# The updateR() command performs the following: finding the latest R version, downloading it, running the installer, deleting the installation file, copy and updating old packages to the new R installation.
+install.packages("installr")
+library(installr)
+updateR()
+
+pkgs<-installed.packages()
+
+
+
+
+
+
+
+
+#
