3bdd198376256af44717c77ab5ef32d302dd4362
Nick <mxr1106go@hotmail.com>
2016-04-13 20:20:43 +0800

    supplement this chapter

--- RinAction/RinA15.R
+++ RinAction/RinA15.R
@@ -1,13 +1,16 @@
 # 处理缺失值
 
+# 安装VIM和mice包
 sapply(c('VIM','mice'),install.packages)
+
 
 data(sleep,package='VIM')
 str(sleep)
 # 识别缺失数据
 sleep[complete.cases(sleep),]
-
 sleep[!complete.cases(sleep),]
+
+complete.cases(sleep)
 
 # 逻辑值TRUE和FALSE分别等价于1和0，可以用sum，mean函数获取缺失值的有用信息
 
@@ -19,6 +22,7 @@ mean(!complete.cases(sleep))
 
 # 探索缺失值模式
 
+# 列表显示缺失值
 # mice包中的md.pattern()可以生成以矩阵或数据框形式展示缺失值模式的表格
 
 library(mice)
@@ -31,13 +35,11 @@ md.pattern(sleep)
 # matrixplot()可以生成每个实例数据的图形，缺失值为红色，用灰度表示数值大小，浅色表示值小。
 # matrixplot()还可以生成一副散点图，
 
-library("VIM")
+library(VIM)
 aggr(sleep, prop = FALSE, numbers = TRUE)
 # numbers = TRUE表示删去数值型的标签
 
-
-
-matrixplot(sleep) # use mouse to sort columns, STOP to move on
+matrixplot(sleep) 
 marginplot(sleep[c("Gest", "Dream")], pch = c(20), 
            col = c("darkgray", "red", "blue"))
 
